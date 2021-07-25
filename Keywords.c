d1080439a8f4aba69ae0221d577f07f5303d7714
MuntasirNahid <69505101+MuntasirNahid@users.noreply.github.com>
2021-07-25 20:54:18 +0600

    Update Keywords.c

--- Keywords.c
+++ Keywords.c
@@ -3,19 +3,22 @@ There are certain reserved words, called keywords, that have standard, predefine
 keywords can be used only for their intended purpose; 
 they cannot be used as programmer-defined identifiers.
 
-The standard keywords are//32 keywords
 
-auto		 extern	 	sizeof
-break 		floatn		 static
-case 		for	 		struct
-char		 goto		 switch
-const		 if	 		typedef
-continue	 int 		union
-default		 long		 unsigned
-do 			register	 void
-double 		return 		volatile
-else 		short		while
-enum    	signed
+There are a total of 44 keywords in C (C89 – 32, C99 – 5, C11 – 7): 
+
+auto        extern        short        while
+break       float         signed        _Alignas
+case        for           sizeof        _Alignof
+char        goto          static        _Atomic
+const       if            struct        _Bool
+continue    inline        switch        _Complex
+default     int           typedef       _Generic
+do          long          union         _Imaginary
+double      register      unsigned      _Noreturn
+else        restrict      void          _Static_assert
+enum        return        volatile      _Thread_local
+
+
 
 Some compilers may also include some or all of the following keywords.
 ada  	far 	near
