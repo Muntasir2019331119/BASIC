bc941e523422c3bde6d85565f7344a82015f84ca
MuntasirNahid <muntasirnahid87@gmail.com>
2021-07-25 23:41:39 +0600

    new

--- Data_Types.c
+++ Data_Types.c
@@ -124,3 +124,53 @@ int main()
 	unsigned char AB=-1;
 	printf("%d",AB);
 }
+
+/*
+##------------------------CONVERSION RULES:-----------------------##
+These rules apply to arithmetic operations between two operators with dissimilar data types. There may be some variation
+from one version of C to another.
+1. If one of the operands is long double, the other will be converted to long double and the result will be long
+double.
+
+2. Otherwise, if one of the operands is double, the other will be converted to double and the result will be double.
+
+3. Otherwise, if one of the operands is f l o a t , the other will be converted to f l o a t and the result will be f l o a t
+
+4. Otherwise, if one of the operands is unsigned long i n t , the other will be converted to unsigned long i n t and
+the result will be unsigned long i n t .
+
+5. Otherwise, if one of the operands is long i n t and the other is unsigned i n t , then:
+(a) If unsigned i n t can be converted to long i n t , the unsigned i n t operand will be converted as such and
+the result will be long i n t .
+
+(b) Otherwise, both operands will be converted to unsigned long i n t and the result will be unsigned long
+i n t .
+
+6. Otherwise, if one of the operands is long i n t , the other will be converted to long i n t and the result will be long
+i n t .
+
+7. Otherwise, if one of the operands is unsigned i n t , the other will be converted to unsigned i n t and the result
+will be unsigned i n t .
+
+8. If none of the above conditions applies, then both operands will be converted to i n t (if necessary), and the result
+will be i n t .
+
+Note that some versions of C automatically convert all floating-point operands to double-precision.
+##----------------------------------------------------------------##
+
+##----------------------ASSIGNMENT RULES:------------------------------##
+
+
+If the two operands in an assignment expression are of different data types, then the value of the right-hand operand will
+automatically be converted to the type of the operand on the left. The entire assignment expression will then be of this
+same data type.
+In addition,
+1. A floating-point value may be truncated if assigned to an integer identifier.
+
+2. A double-precision value may be rounded if assigned to a floating-point (single-precision) identifier.
+
+3. An integer quantity may be altered (some high-order bits may be lost) if it is assigned to a shorter integer identifier
+or to a character identifier .
+
+##------------------------------------------------------------------##
+*/
---
+++ Operators.c
@@ -0,0 +1,84 @@
+/*
+
+Operators are the foundation of any programming language.
+We can define operators as symbols that help us to perform specific mathematical and logical computations on operands.
+ In other words, we can say that an operator operates the operands.
+
+ 1)Arithmetic Operators (+, -, *, /, %, post-increment, pre-increment, post-decrement, pre-decrement)
+
+2)Relational Operators (==, !=, >, <, >= & <=) Logical Operators (&&, || and !)
+
+3)Bitwise Operators (&, |, ^, ~, >> and <<)
+
+4)Assignment Operators (=, +=, -=, *=, etc)
+
+5)Other Operators (conditional, comma, sizeof, address, redirection)
+
+1) Arithmetic Operators: These are the operators used to perform arithmetic/mathematical operations on operands. Examples: (+, -, *, /, %,++,–). Arithmetic operator are of two types:
+      Unary Operators: Operators that operates or works with a single operand are unary operators. For example: (++ ,--)
+
+      Binary Operators: Operators that operates or works with two operands are binary operators. For example: (+ , – , * , /)
+
+2)Relational Operators: These are used for comparison of the values of two operands. For example, checking if one operand is equal to the other operand or not, an operand is greater than the other operand or not etc. Some of the relational operators are (==, >= , <= ).
+
+3)Logical Operators:  Logical Operators are used to combine two or more conditions/constraints or to complement the evaluation of the original condition in consideration. The result of the operation of a logical operator is a boolean value either true or false. For example, the logical AND represented as ‘&&’ operator in C or C++ returns true when both the conditions under consideration are satisfied. Otherwise it returns false. Therfore, a && b returns true when both a and b are true (i.e. non-zero)
+
+4)Bitwise Operators: The Bitwise operators is used to perform bit-level operations on the operands. The operators are first converted to bit-level and then the calculation is performed on the operands. The mathematical operations such as addition, subtraction, multiplication etc. can be performed at bit-level for faster processing. For example, the bitwise AND represented as & operator in C or C++ takes two numbers as operands and does AND on every bit of two numbers. The result of AND is 1 only if both bits are 1.
+
+5)Assignment Operators: Assignment operators are used to assign value to a variable. The left side operand of the assignment operator is a variable and right side operand of the assignment operator is a value. The value on the right side must be of the same data-type of variable on the left side otherwise the compiler will raise an error.
+Example: +=,-=,*=,/=,
+
+
+##-------------------Precedence------------------------------------##
+Category			Operator							Associativity
+--------			--------							-------------
+Postfix				() [] -> . ++ --					Left to right
+Unary				+ - ! ~ ++ - - (type)* & sizeof		Right to left
+Multiplicative		* / %								Left to right
+Additive			+ -									Left to right
+Shift				<< >>								Left to right
+Relational			< <= > >=							Left to right
+Equality			== !=								Left to right
+Bitwise AND			&									Left to right
+Bitwise XOR			^									Left to right
+Bitwise OR			|									Left to right
+Logical AND			&&									Left to right
+Logical OR			||									Left to right
+Conditional			?:									Right to left
+Assignment			= += -= *= /= %=>>= <<= &= ^= |=	Right to left
+Comma				,									Left to right
+
+
+Example:
+#include <stdio.h>
+
+int main() 
+{
+
+   int a = 20;
+   int b = 10;
+   int c = 15;
+   int d = 5;
+   int e;
+ 
+   e = (a + b) * c / d;      // ( 30 * 15 ) / 5
+   printf("Value of (a + b) * c / d is : %d\n",  e );
+
+   e = ((a + b) * c) / d;    // (30 * 15 ) / 5
+   printf("Value of ((a + b) * c) / d is  : %d\n" ,  e );
+
+   e = (a + b) * (c / d);   // (30) * (15/5)
+   printf("Value of (a + b) * (c / d) is  : %d\n",  e );
+
+   e = a + (b * c) / d;     //  20 + (150/5)
+   printf("Value of a + (b * c) / d is  : %d\n" ,  e );
+  
+   return 0;
+}
+When you compile and execute the above program, it produces the following result −
+
+Value of (a + b) * c / d is : 90
+Value of ((a + b) * c) / d is  : 90
+Value of (a + b) * (c / d) is  : 90
+Value of a + (b * c) / d is  : 50
+*/
